d3ba461411e325d1e0dee8268a5052e014b90556
aishahkabir <aishahkab@gmail.com>
2021-08-12 14:03:09 -0400

    Fix timer in main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-import datetime
-import time
+from datetime import datetime
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -8,11 +7,12 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        self.t = 1500
 
     def create_widgets(self):
-        #self.time_now = datetime.now().strftime("%H:%M:%S")
+        self.time_now = datetime.now().strftime("%H:%M:%S")
         self.label = tk.Label(self)
-        #self.label["text"] = self.time_now
+        self.label["text"] = self.time_now
         self.label.pack()
         self.start = tk.Button(self, text="Start timer", fg="blue")
         self.start.pack(side="left")
@@ -23,36 +23,18 @@ class Application(tk.Frame):
         self.quit.pack(side="bottom")
 
     def update(self):
-        #self.label["text"] = datetime.now().strftime("%H:%M:%S")
-        root.after(1000, self.update)
-
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-def _format_time(t):
-    seconds = t % (24 * 3600)
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-
-    return "%02d:%02d" % (minutes, seconds)
-
-def countdown(t):
-    while t:
-        timer = str(datetime.timedelta(seconds=t))
-        print(_format_time(t))
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-
-    print('Fire in the hole!!')
-
+        if self.t > 0:
+            seconds = self.t % (24 * 3600)
+            seconds %= 3600
+            minutes = seconds // 60
+            seconds %= 60
+            self.label["text"] = "%02d:%02d" % (minutes, seconds)
+            self.t -= 1
+            root.after(1000, self.update)
 
 root = tk.Tk()
 root.title("Pomodoro Timer")
 root.geometry("500x200")
 app = Application(master=root)
-countdown(int(1500))
 app.update()
 app.mainloop()
